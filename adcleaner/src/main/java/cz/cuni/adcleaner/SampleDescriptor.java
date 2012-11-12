dad5265cf2ae82362fb8b80467d4b01b3e584bb5
zviri <zviri@zviri-Vostro-V131.(none)>
2012-11-12 11:22:13 +0100

    Add JFeatureLib and xuggle dependency

---
+++ adcleaner/src/main/java/cz/cuni/adcleaner/SampleDescriptor.java
@@ -0,0 +1,35 @@
+package cz.cuni.adcleaner;
+
+import ij.process.ImageProcessor;
+
+import org.apache.log4j.Logger;
+
+import de.lmu.dbs.jfeaturelib.Progress;
+import de.lmu.dbs.jfeaturelib.features.AbstractFeatureDescriptor;
+
+/**
+ * This is a sample descriptor
+ *
+ * @author Franz
+ */
+public class SampleDescriptor extends AbstractFeatureDescriptor {
+
+    static final Logger log = Logger.getLogger(SampleDescriptor.class.getName());
+
+    @Override
+    public String getDescription() {
+        return "A sample descript that just returns some arbitrary numbers";
+    }
+
+    @Override
+    public void run(ImageProcessor ip) {
+        firePropertyChange(Progress.START);
+
+        // if you want to extract multiple features for one image just add as much 
+        // feature vectors as you want
+        double[] featureVector = {22d, 03d, 1980d};
+        addData(featureVector);
+
+        firePropertyChange(Progress.END);
+    }
+}
